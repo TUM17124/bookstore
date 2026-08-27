9d6eb2fd8c7f77eb07d2bb053f54ecc931a92ea6
TUM17124 <tumgodwinkiprotich@gmail.com>
2026-08-27 17:06:02 +0300

    MADE HOME APPEAR SLIGTLY DOWN ABOVE NAV BAR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
            
           <BookmarksProvider>
           <NotchNavbar />
-          <div className="site-main-offset pt-10">{children}</div>
+          <div className="site-main-offset pt-19">{children}</div>
           <SiteFooter />
           </BookmarksProvider>
           
